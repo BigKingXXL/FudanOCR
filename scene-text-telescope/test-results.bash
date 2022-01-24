58736d494b4e515244b38117bc626668c85eaae4
Maximilian Schulze <maximilian.schulze@student.hpi.de>
2022-01-24 15:58:26 +0100

    tee

--- scene-text-telescope/test-results.bash
+++ scene-text-telescope/test-results.bash
@@ -3,7 +3,7 @@
 for filename in checkpoint/$1/epoch*_.pth; do
     echo "=============================="
     echo $filename
-    bash -c "WANDB_MODE=disabled python3 main.py --resume $filename --text_focus --STN --test --batch_size=16 --exp_name $1 --test_data_dir ./dataset/mydata/test"
+    bash -c "WANDB_MODE=disabled python3 main.py --resume $filename --text_focus --STN --test --batch_size=16 --exp_name $1 --test_data_dir ./dataset/mydata/test | tee log.txt"
     echo ""
     echo ""
 done
