f39ada5ae3f05be69b8843d71f6b873a29b4d861
Maximilian Schulze <maximilian.schulze@student.hpi.de>
2022-01-24 17:07:41 +0100

    resnext

--- scene-text-telescope/model/tbsrn.py
+++ scene-text-telescope/model/tbsrn.py
@@ -5,6 +5,7 @@ from torch import nn
 import warnings
 import math, copy
 from torch.quantization import QuantStub, DeQuantStub
+from timm.models.layers import trunc_normal_, DropPath
 
 warnings.filterwarnings("ignore")
 
@@ -15,6 +16,43 @@ def clones(module, N):
     "Produce N identical layers."
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
 
+
+class Block(nn.Module):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    We use (2) as we find it slightly faster in PyTorch
+    
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
 
 class LayerNorm(nn.Module):
     "Construct a layernorm module (See citation for details)."
@@ -169,6 +207,7 @@ class TBSRN(nn.Module):
         self.quant = QuantStub()
         self.dequant = DeQuantStub()
         self.f_add = torch.nn.quantized.FloatFunctional()
+        self.resnext_block = Block(3)
         in_planes = 3
         if mask:
             in_planes = 4
@@ -215,6 +254,8 @@ class TBSRN(nn.Module):
 
     def forward(self, x):
         print("Size in beginning of forward: ", x.size())
+        resnext = self.resnext_block(x)
+        print("Size after block", resnext.size())
         if self.quantize:
             x = self.quant(x)
         if self.stn and self.training:
@@ -222,6 +263,7 @@ class TBSRN(nn.Module):
             _, ctrl_points_x = self.stn_head(x)
             x, _ = self.tps(x, ctrl_points_x)
         print("Size after stn: ", x.size())
+        
 
         #apply first block
         block = {'1': self.block1(x)}
