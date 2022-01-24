da58275196963a26e02ac78e4ada942363600eb2
Maximilian Schulze <maximilian.schulze@student.hpi.de>
2022-01-24 15:45:32 +0100

    textfocus

--- scene-text-telescope/test-results.bash
+++ scene-text-telescope/test-results.bash
@@ -3,7 +3,7 @@
 for filename in checkpoint/$1/epoch*_.pth; do
     echo "=============================="
     echo $filename
-    bash -c "python3 main.py --resume $filename --text_zoom --STN --test --batch_size=16 --exp_name $1 --test_data_dir ./dataset/mydata/test"
+    bash -c "python3 main.py --resume $filename --text_focus --STN --test --batch_size=16 --exp_name $1 --test_data_dir ./dataset/mydata/test"
     echo ""
     echo ""
 done
